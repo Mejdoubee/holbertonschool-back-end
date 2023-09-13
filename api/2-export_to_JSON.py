81f53cab83d5e9ddb4881fb0bedfb007d9afe73a
Mejdoubee <6235@holbertonstudents.com>
2023-09-13 01:17:28 +0100

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -5,15 +5,16 @@ This module defines the REST API.
 import json
 import requests
 from sys import argv
+
 BASE_URL = 'https://jsonplaceholder.typicode.com'
 
 
-def get_name(id):
-    '''Fetch employee name by ID'''
+def get_username(id):
+    '''Fetch employee username by ID'''
     response = requests.get(f'{BASE_URL}/users/{id}')
     response.raise_for_status()
     user_data = response.json()
-    return user_data.get('name')
+    return user_data.get('username')
 
 
 def get_todos(id):
@@ -23,30 +24,10 @@ def get_todos(id):
     return response.json()
 
 
-def display_todo(id):
-    '''Display the TODO list progress for the given employee ID'''
-    try:
-        employee_name = get_name(id)
-        todos = get_todos(id)
-
-        completed_tasks = [task for task in todos if task['completed']]
-        completed_count = len(completed_tasks)
-        total_tasks = len(todos)
-
-        print(f"Employee {employee_name} is done "
-              f"with tasks({completed_count}/{total_tasks}):")
-
-        for task in completed_tasks:
-            print(f"\t {task['title']}")
-
-    except requests.RequestException as e:
-        print(f"An error occurred: {e}")
-
-
 def export_to_json(id):
     '''Export the TODO list to JSON for the given employee ID'''
     try:
-        employee_username = get_name(id)
+        employee_username = get_username(id)
         todos = get_todos(id)
 
         tasks_list = []
@@ -58,6 +39,7 @@ def export_to_json(id):
             }
             tasks_list.append(task_data)
 
+        # Construct the final JSON structure
         json_structure = {str(id): tasks_list}
         with open(f"{id}.json", "w") as json_file:
             json.dump(json_structure, json_file)
